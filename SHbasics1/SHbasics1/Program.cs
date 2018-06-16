5957aaedb24b50bdb94a6d18f25b9d380c8fba2b
alexandrbektashev <alexandr.bektashev@gmail.com>
2018-06-17 00:40:35 +0300

    IerarchyStructure initial commit

---
+++ SHbasics1/SHbasics1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHbasics1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //в общем есть иерархия 
+
+            //надо сделать эту иерархию записанной в файл 
+
+            //допустим есть три уровня
+
+            
+
+
+        }
+    }
+}
